dfc2b59e63d87a01ab4de9dbb9c65c260d77366a
Abraham0912 <09120912ahn@gmail.com>
2020-12-16 12:04:35 -0600

    saludadorUpdateToSetOne

--- setOne/script.js
+++ setOne/script.js
@@ -1,8 +1,7 @@
-
+//INSTRUCCIONES: DESCOMENTE UN BLOQUE DE CODIGO PARA VER EL FUNCIONAMIENTO
 
 /* let nombre = prompt("Dame tu nombre completo");
 document.writeln(`hola  ${nombre}`); */
-
 
 /* let numero1 =parseInt(prompt("Dame un numero"));
 let numero2 =parseInt(prompt("Dame un numero"));
@@ -11,8 +10,8 @@ document.writeln(`el resultado de la suma es ${numero1 + numero2}`); */
 let numero1 =parseInt(prompt("Dame un numero"));
 let numero2 =parseInt(prompt("Dame un numero"));
 if(numero1>numero2){
-    document.writeln(`El primer numero ingresado es mayor al segundo`);    
-}if(numero1==numero2){
-    document.writeln("Los numero son iguales");
+    document.writeln(`El numero <b>${numero1}</b> es mayor al numero <b>${numero2}</b>`);    
+}else if(numero1==numero2){
+    document.writeln(`El numero <b>${numero1}</b> es igual al numero <b>${numero2}</b>`);
 }else{
-document.writeln(`El segundo numero es mayor al primero`);}
+document.writeln(`El numero <b> ${numero1}</b> es MENOR al numero <b>${numero2}</b>`);}
