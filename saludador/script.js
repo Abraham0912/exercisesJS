3896bda5202c3729309b4498dcc735e8d5d9a0b0
Abraham0912 <09120912ahn@gmail.com>
2020-12-16 12:07:55 -0600

    dropFilePHP

---
+++ saludador/script.js
@@ -0,0 +1,18 @@
+//INSTRUCCIONES: DESCOMENTE UN BLOQUE DE CODIGO PARA VER EL FUNCIONAMIENTO
+
+/* let nombre = prompt("Dame tu nombre completo");
+document.writeln(`hola  ${nombre}`); */
+
+/* let numero1 =parseInt(prompt("Dame un numero"));
+let numero2 =parseInt(prompt("Dame un numero"));
+document.writeln(`el resultado de la suma es ${numero1 + numero2}`); */
+
+/* let numero1 =parseInt(prompt("Dame un numero"));
+let numero2 =parseInt(prompt("Dame un numero"));
+if(numero1>numero2){
+    document.writeln(`El numero <b>${numero1}</b> es mayor al numero <b>${numero2}</b>`);    
+}else if(numero1==numero2){
+    document.writeln(`El numero <b>${numero1}</b> es igual al numero <b>${numero2}</b>`);
+}else{
+document.writeln(`El numero <b>${numero1}</b> es MENOR al numero <b>${numero2}</b>`);} */
+
