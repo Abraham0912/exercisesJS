4f97ae6b199c9a04713d85d9fb785ed68c360141
Abraham0912 <09120912ahn@gmail.com>
2020-12-16 11:35:49 -0600

    firstFiles

---
+++ saludador/script.js
@@ -0,0 +1,18 @@
+
+
+/* let nombre = prompt("Dame tu nombre completo");
+document.writeln(`hola  ${nombre}`); */
+
+
+/* let numero1 =parseInt(prompt("Dame un numero"));
+let numero2 =parseInt(prompt("Dame un numero"));
+document.writeln(`el resultado de la suma es ${numero1 + numero2}`); */
+
+let numero1 =parseInt(prompt("Dame un numero"));
+let numero2 =parseInt(prompt("Dame un numero"));
+if(numero1>numero2){
+    document.writeln(`El primer numero ingresado es mayor al segundo`);    
+}if(numero1==numero2){
+    document.writeln("Los numero son iguales");
+}else{
+document.writeln(`El segundo numero es mayor al primero`);}
