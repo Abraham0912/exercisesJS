d54f791d26bf090ff71474d543c82e2968a2fe14
Abraham0912 <09120912ahn@gmail.com>
2020-12-16 20:23:08 -0600

    testConcatFunction

--- setOne/script.js
+++ setOne/script.js
@@ -1,12 +1,15 @@
 //INSTRUCCIONES: DESCOMENTE UN BLOQUE DE CODIGO PARA VER EL FUNCIONAMIENTO
 
-/* let nombre = prompt("Dame tu nombre completo");
-document.writeln(`hola  ${nombre}`); */
+/* Exercise_1
+let nombre = prompt("Dame tu nombre completo");
+document.writeln(`hola  ${nombre}`); endExercise*/
 
-/* let numero1 =parseInt(prompt("Dame un numero"));
+/* Exercise_2
+let numero1 =parseInt(prompt("Dame un numero"));
 let numero2 =parseInt(prompt("Dame un numero"));
-document.writeln(`el resultado de la suma es ${numero1 + numero2}`); */
+document.writeln(`el resultado de la suma es ${numero1 + numero2}`); endExercise*/
 
+/* Exercise_3
 let numero1 =parseInt(prompt("Dame un numero"));
 let numero2 =parseInt(prompt("Dame un numero"));
 if(numero1>numero2){
@@ -14,4 +17,24 @@ if(numero1>numero2){
 }else if(numero1==numero2){
     document.writeln(`El numero <b>${numero1}</b> es igual al numero <b>${numero2}</b>`);
 }else{
-document.writeln(`El numero <b> ${numero1}</b> es MENOR al numero <b>${numero2}</b>`);}
+document.writeln(`El numero <b> ${numero1}</b> es MENOR al numero <b>${numero2}</b>`);} endExercise*/
+
+/* Exercise_4
+let numero1 = parseInt(prompt('Dame numero'));
+let numero2 = parseInt(prompt('Dame numero'));
+let numero3 = parseInt(prompt('Dame nuemro'));
+let numeros = 10;
+document.writeln(`Los numeros son ${numero1}, ${numero1}, ${numero1}, ${numeros}<br> `); endExercise*/
+
+//let sumatoriaAreglos = arreglo.concat(arreglo2, arreglo3); usando CONCAT()
+
+
+let cadena = 'cadena de prueba';
+let cadena2  = ' de ';
+let resultado = cadena.indexOf(cadena2);
+
+document.writeln(resultado + ' - ');
+
+var array = [2, 9, 9];
+
+document.writeln(array.indexOf(2));//array.indexOf(searchElement['tres', 1]);
