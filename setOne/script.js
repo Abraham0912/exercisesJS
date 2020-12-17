8a9defa7139e0f2b4dba4aa4b9c15edda9e9fa18
Abraham0912 <09120912ahn@gmail.com>
2020-12-16 20:44:39 -0600

    endExercise4

--- setOne/script.js
+++ setOne/script.js
@@ -19,22 +19,12 @@ if(numero1>numero2){
 }else{
 document.writeln(`El numero <b> ${numero1}</b> es MENOR al numero <b>${numero2}</b>`);} endExercise*/
 
-/* Exercise_4
+/* Exercise_4*/
 let numero1 = parseInt(prompt('Dame numero'));
 let numero2 = parseInt(prompt('Dame numero'));
 let numero3 = parseInt(prompt('Dame nuemro'));
-let numeros = 10;
-document.writeln(`Los numeros son ${numero1}, ${numero1}, ${numero1}, ${numeros}<br> `); endExercise*/
+let numeros = [numero1,numero2,numero3];
+document.writeln(`<br>Imprimiendo array: ${numeros}`);
+document.writeln(`<br>Imprimiendo array ordenado: ${numeros.sort().reverse()} `);
+/*endExersise */
 
-//let sumatoriaAreglos = arreglo.concat(arreglo2, arreglo3); usando CONCAT()
-
-
-let cadena = 'cadena de prueba';
-let cadena2  = ' de ';
-let resultado = cadena.indexOf(cadena2);
-
-document.writeln(resultado + ' - ');
-
-var array = [2, 9, 9];
-
-document.writeln(array.indexOf(2));//array.indexOf(searchElement['tres', 1]);
