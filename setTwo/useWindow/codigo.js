58ed5d3bb0d16c0ed882366c063cdae369e6344d
Abraham0912 <09120912ahn@gmail.com>
2020-12-22 15:14:46 -0600

    endExerciseUseWindow

--- setTwo/useWindow/codigo.js
+++ setTwo/useWindow/codigo.js
@@ -1,1 +1,12 @@
-alert("Estoy conectado con index");
+console.log("Estoy conectado con index.html");
+let alto = window.screen.height;
+let ancho = window.screen.width;
+let comprar = confirm(`¿Quieres realizar la compra?
+Info de la resolucion de la ventana:
+Ancho = ${ancho} Alto =  ${alto}`);
+
+if(comprar){
+    alert("Felicidades el proceso de compra continuara")
+}else{
+    alert("El proceso de compra se detuvo :C")
+}
