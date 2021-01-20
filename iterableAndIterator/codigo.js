b956bd7217a35a4510670e33ea8dd83c0653be42
Abraham0912 <09120912ahn@gmail.com>
2021-01-20 16:00:19 -0600

    endIterableAndInterator

--- iterableAndIterator/codigo.js
+++ iterableAndIterator/codigo.js
@@ -1,1 +1,11 @@
-alert("Estoy conectado con html");
+
+const iterable = [1,2,3,4,5];
+//const iterable = "Hola mundo";
+
+
+const iterador = iterable[Symbol.iterator]();
+
+console.log(iterable);//Hola mundo
+console.log(iterador);//>StringIterator{}
+
+console.log(iterador.next);//>StringIterator{}
